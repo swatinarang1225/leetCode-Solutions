3c1cac17e51250cd87185dbdac2fbc18d1e6c62d
Swati Narang <swatinarang1225@gmail.com>
2022-10-07 16:33:07 +0530

    Create 732. My Calendar III.py

---
+++ 732. My Calendar III.py
@@ -0,0 +1,22 @@
+from sortedcontainers import SortedDict
+
+
+class MyCalendarThree:
+
+    def __init__(self):
+        self.diff = SortedDict()
+
+    def book(self, start: int, end: int) -> int:
+        self.diff[start] = self.diff.get(start, 0) + 1
+        self.diff[end] = self.diff.get(end, 0) - 1
+        cur = res = 0
+        for delta in self.diff.values():
+            cur += delta
+            res = max(cur, res)
+        return res
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
