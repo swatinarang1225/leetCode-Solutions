147e7e4b1fe6fef5a64d66cb0efc64a5ebfde0d4
Swati Narang <swatinarang1225@gmail.com>
2023-09-27 15:08:37 +0530

    Create 476. Number Complement.cpp

---
+++ 476. Number Complement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findComplement(int num) {
+
+        if (num == 0){
+            return 1;
+        }
+        int m = num;
+        int mask = 0;
+
+        while(m !=0){
+            mask = mask <<1 |1;
+            m = m>>1;
+        }
+
+        int ans = (~num) & mask;
+
+        return ans;
+
+
+        
+    }
+};
